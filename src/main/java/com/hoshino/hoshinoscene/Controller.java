79cad23e7ccafdb6853432b107b257c437e4ea44
GitBADASS <2606452878@qq.com>
2022-12-10 23:26:00 +0800

    change css

--- src/main/java/com/hoshino/hoshinoscene/Controller.java
+++ src/main/java/com/hoshino/hoshinoscene/Controller.java
@@ -1,25 +1,20 @@
 package com.hoshino.hoshinoscene;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class Controller  {
-    @FXML
-    private Label welcomeText;
+    /*@FXML
+    private Label welcomeText;*/
 
     @FXML
     private Button min;
 
-    @FXML
+    /*@FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+        welcomeText.setText("Welcome to Hoshino Application!");
+    }*/
 
     @FXML
     protected void exit() {
