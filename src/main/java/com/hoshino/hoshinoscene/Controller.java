739e985b0485df03448aaf134b8b74786b4fab04
GitBADASS <2606452878@qq.com>
2022-12-17 10:40:20 +0800

    add the button to create words' warehouse

--- src/main/java/com/hoshino/hoshinoscene/Controller.java
+++ src/main/java/com/hoshino/hoshinoscene/Controller.java
@@ -1,9 +1,12 @@
 package com.hoshino.hoshinoscene;
 
+import com.hoshino.hoshinoscene.tools.GenWarehouse;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,10 +17,22 @@ public class Controller implements Initializable{
     * 要求：点击创建库按钮进入创建学习库窗口
     * 我们应该提前先创建好一个窗口，然后在触发点击事件的时候调用show()方法，否则会出现多开bug
     * */
+    //创建一个新窗口
+    GenWarehouse gen = new GenWarehouse();
+
+    public Controller() throws IOException {
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        //创建库：创建新窗口
         createWarehouse.setOnMousePressed(e->{
-
+            //检查新窗口是否已存在，若为否，则新建，若为是，则将它拽到前面
+            if(!gen.isShowing()) {
+                gen.show();
+            } else {
+                gen.toFront();
+            }
         });
     }
 }
---
+++ src/main/java/com/hoshino/hoshinoscene/tools/GenWarehouse.java
@@ -0,0 +1,19 @@
+package com.hoshino.hoshinoscene.tools;
+
+import com.hoshino.hoshinoscene.HoshinoApp;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class GenWarehouse extends Stage {
+    public GenWarehouse() throws IOException {
+        Scene sc = new Scene(new FXMLLoader(GenWarehouse.class.getResource("pages/GenWrh.fxml")).load());
+        this.setScene(sc);
+        this.setTitle("Hoshino");
+        this.setResizable(false);
+        this.getIcons().add(new Image(Objects.requireNonNull(HoshinoApp.class.getResourceAsStream("icons/little.png"))));
+    }
+}
