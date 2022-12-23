ea2a0e9e35c8bf8e3b3dc201d1e6b3ba94657742
GitBADASS <2606452878@qq.com>
2022-12-23 14:58:00 +0800

    keep completing the saving function

--- src/main/java/com/hoshino/hoshinoscene/custom/WarehouseStyle.java
+++ src/main/java/com/hoshino/hoshinoscene/custom/WarehouseStyle.java
@@ -4,9 +4,9 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class WarehouseStyle extends VBox {
-    public WarehouseStyle(String title, String description, int index) {
+    public WarehouseStyle(String title, String description) {
         Label titleText = new Label(title);
-        setId(String.valueOf(index));//index变量分析：添加右键菜单选择删除的时候，将这个id作为索引删除Storage.wh对应元素
+        setId(title);//设置ID为title方便日后操作（比如删除、防重等
         Label descriptionText = new Label(description);
         Label test = new Label();
         getChildren().add(titleText);
@@ -14,4 +14,6 @@ public class WarehouseStyle extends VBox {
         getChildren().add(test);
         setOnMousePressed(e->test.setText("NONONONONO"));
     }
+
+    //TODO:考虑：是否要创建几个方法专门用于删除、设置（比如重命名、重新定义描述等）？
 }
--- src/main/java/com/hoshino/hoshinoscene/tools/Storage.java
+++ src/main/java/com/hoshino/hoshinoscene/tools/Storage.java
@@ -2,25 +2,42 @@ package com.hoshino.hoshinoscene.tools;
 
 
 import com.alibaba.fastjson2.JSONObject;
-
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
+//存储类，负责存储数据以及数据通信
 public class Storage {
 
+    //创建一个JSON对象用于存放学习库信息
     static JSONObject warehouse = new JSONObject();
 
-
-    public void save(WordsWarehouse wh) {
+    //保存方法，传入一个要保存的学习库类，此方法为工具方法，所以设为静态
+    public static void save(WordsWarehouse wh) {
+        //将该学习库信息存储进JSON对象中
         warehouse.put(wh.getName(), wh);
+        //将该JSON对象转为字符串以便后面写入
+        String s = warehouse.toJSONString();
+        //创建JSON文件并写入
+        try {
+            FileOutputStream wos = new FileOutputStream(wh.getName()+".json");//开放一个文件输出流（文件名用学习库类的“name”以便日后的操作，文件格式为JSON
+            OutputStreamWriter writer = new OutputStreamWriter(wos, StandardCharsets.UTF_8);//构建OutputStreamWriter对象以兼容编码
+            writer.append(s);//写入到文件
+            writer.close();//关闭 OutputStreamWriter ，保存
+            wos.close();//关闭文件输出流，释放资源
+        } catch (IOException e) {
+            System.out.print("JSON文件创建和写入过程出错");//出错时提示
+        }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    /*public static void main(String[] args) throws FileNotFoundException {
         HashMap<String, String> h = new HashMap<>();
         h.put("test", "测试");
-        WordsWarehouse w = new WordsWarehouse("Test", "it is a test", h);
+        WordsWarehouse w = new WordsWarehouse("Test", "it is a test warehouse", h);
         warehouse.put("test", w);
-    }
+        String s = warehouse.toJSONString();
+        System.out.println(s + "\n" + s.length());
+    }*/
 
 }
