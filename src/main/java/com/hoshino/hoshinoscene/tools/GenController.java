827aace3ceb0416d3dd6506f9675335e1385555e
GitBADASS <2606452878@qq.com>
2022-12-27 12:01:37 +0800

    completing the function of saving words

--- src/main/java/com/hoshino/hoshinoscene/tools/GenController.java
+++ src/main/java/com/hoshino/hoshinoscene/tools/GenController.java
@@ -37,10 +37,11 @@ public class GenController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         List<HBox> elementsArrayList = new ArrayList<>();
         addElement.setOnAction(e->{
+            //TODO:这个到时候自己写一个HBox的子类在这里用，该子类特点：含有两个 TextFiled、一个 checkbox，然后ID待定
             HBox h = new HBox();
             h.setStyle("-fx-background-color: gray; -fx-pref-width: 300px; -fx-pref-height: 20px");
             Label text = new Label();
-            text.setText("测试");//TODO:这个到时候自己写一个HBox的子类在这里用，该子类特点：含有两个 TextFiled、一个 checkbox，然后ID待定
+            text.setText("测试");
             h.getChildren().add(text);
             elementsArrayList.add(h);
             ObservableList<HBox> el = FXCollections.observableList(elementsArrayList);
