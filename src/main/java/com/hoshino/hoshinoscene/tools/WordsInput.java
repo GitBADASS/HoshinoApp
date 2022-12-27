ba7fd853ab5a388798366ec44f2f12ccd9615b79
GitBADASS <2606452878@qq.com>
2022-12-27 12:17:49 +0800

    modified a little

--- src/main/java/com/hoshino/hoshinoscene/tools/WordsInput.java
+++ src/main/java/com/hoshino/hoshinoscene/tools/WordsInput.java
@@ -1,17 +1,19 @@
 package com.hoshino.hoshinoscene.tools;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 public class WordsInput extends HBox {
-    private static final String TF_STYLE = "-fx-background-color: none; -fx-pref-width: 120px; -fx-pref-height: 20px; -fx-border-color: #a8a8a8; -fx-border-width: 0 0 0 1";
+    private static final String TF_STYLE_LEFT = "-fx-background-color: none; -fx-pref-width: 120px; -fx-pref-height: 20px; -fx-border-color: #a8a8a8; -fx-border-width: 0 1 0 0";
+    private static final String TF_STYLE = "-fx-background-color: none; -fx-pref-width: 120px; -fx-pref-height: 20px;";
     public WordsInput() {
-        CheckBox ck = new CheckBox();//TODO:待完成checkbox相关功能
+        setAlignment(Pos.CENTER);
         TextField cn = new TextField();
         TextField en = new TextField();
-        cn.setStyle(TF_STYLE);
+        cn.setStyle(TF_STYLE_LEFT);
         en.setStyle(TF_STYLE);
-        this.getChildren().addAll(cn, en, ck);
+        this.getChildren().addAll(cn, en);
     }
 }
