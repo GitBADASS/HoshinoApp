b4eb8377229e5d96039acb5366f5d6dde1d07fd2
GitBADASS <2606452878@qq.com>
2022-12-30 17:48:04 +0800

    prepare to complete the function of showing warehouse(s)

--- src/main/java/com/hoshino/hoshinoscene/Controller.java
+++ src/main/java/com/hoshino/hoshinoscene/Controller.java
@@ -1,14 +1,20 @@
 package com.hoshino.hoshinoscene;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.hoshino.hoshinoscene.tools.GenWarehouse;
+import com.hoshino.hoshinoscene.tools.WordsWarehouse;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
@@ -49,7 +55,20 @@ public class Controller implements Initializable{
     }
 
     //加载文件并展示
-    public void load() {
+    public static void load() throws IOException {
+        //遍历文件夹读取文件
+        File jsons = new File("json\\warehouses");
+        File[] list = jsons.listFiles();//获取库文件夹下所有文件
+        assert list != null;
+        for (File file : list) {
+            String content = new String(Files.readAllBytes(Paths.get(file.toURI())));//获取文件内容
+            //String jContent = JSONObject.toJSONString(content);
+            //WordsWarehouse wordsWarehouse = JSON.parseObject(jContent, WordsWarehouse.class);
+            //System.out.println(wordsWarehouse);
+        }
+    }
 
+    public static void main(String[] args) throws IOException {
+        load();
     }
 }
