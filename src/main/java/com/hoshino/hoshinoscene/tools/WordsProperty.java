4253785a79a1216f14a66efc95b711000d500637
GitBADASS <2606452878@qq.com>
2023-01-25 00:12:34 +0800

    prepare to add a new property to a word

---
+++ src/main/java/com/hoshino/hoshinoscene/tools/WordsProperty.java
@@ -0,0 +1,34 @@
+package com.hoshino.hoshinoscene.tools;
+
+public class WordsProperty {
+
+    int type;
+
+    String translation;
+
+    String fixedTranslation;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(String translation) {
+        this.translation = translation;
+    }
+
+    public String getFixedTranslation() {
+        return fixedTranslation;
+    }
+
+    public void setFixedTranslation(String fixedTranslation) {
+        this.fixedTranslation = fixedTranslation;
+    }
+}
