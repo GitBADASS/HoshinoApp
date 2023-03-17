9d54bf11397666bce61cc09edb3c13173803ff09
GitBADASS <2606452878@qq.com>
2023-03-17 22:24:51 +0800

    test new effect

---
+++ src/main/java/com/hoshino/hoshinoscene/tools/animations/TypingEffect.java
@@ -0,0 +1,42 @@
+package com.hoshino.hoshinoscene.tools.animations;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class TypingEffect extends Application {
+
+    private int counter = 0;
+    private String text = "Hello, World!";
+    private Label label = new Label("");
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Typing Effect Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(80), event -> {
+            if (counter <= text.length()) {
+                label.setText(text.substring(0, counter));
+                counter++;
+            }
+        }));
+        timeline.setCycleCount(text.length() + 1);
+        timeline.play();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.hoshino.hoshinoscene {
     requires javafx.controls;
+    requires javafx.graphics;
     requires javafx.fxml;
     requires com.alibaba.fastjson2;
 
@@ -8,6 +9,7 @@ module com.hoshino.hoshinoscene {
     opens com.hoshino.hoshinoscene.tools to javafx.fxml, com.alibaba.fastjson2;
     exports com.hoshino.hoshinoscene;
     exports com.hoshino.hoshinoscene.tools;
+    exports com.hoshino.hoshinoscene.tools.animations;
     exports com.hoshino.hoshinoscene.controllers;
     exports com.hoshino.hoshinoscene.custom;
     opens com.hoshino.hoshinoscene.controllers to com.alibaba.fastjson2, javafx.fxml;
