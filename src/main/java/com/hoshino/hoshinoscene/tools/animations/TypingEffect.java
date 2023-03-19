d207c8e0e1d4b379159247862d10e6fc0e417084
GitBADASS <2606452878@qq.com>
2023-03-19 18:41:13 +0800

    test new effect

--- src/main/java/com/hoshino/hoshinoscene/tools/animations/TypingEffect.java
+++ src/main/java/com/hoshino/hoshinoscene/tools/animations/TypingEffect.java
@@ -12,7 +12,7 @@ import javafx.util.Duration;
 public class TypingEffect extends Application implements TextEffect {
 
     private int counter = 0;
-    private String text = "Hello, World!";
+    private String text = "Hello, World! Typing Effect Demo";
     private Label label = new Label("");
 
     @Override
@@ -20,20 +20,37 @@ public class TypingEffect extends Application implements TextEffect {
         StackPane root = new StackPane();
         root.getChildren().add(label);
 
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 800, 600);
 
         primaryStage.setTitle("Typing Effect Demo");
         primaryStage.setScene(scene);
         primaryStage.show();
+        final int[] e = {0};
 
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(80), event -> {
+        Timeline in = new Timeline(new KeyFrame(Duration.millis(80), event -> {
             if (counter <= text.length()) {
-                label.setText(text.substring(0, counter));
+                //label.setStyle("-fx-background-color: black; -fx-text-fill: white");
+                if (e[0] == 0) {
+                    label.setText(text.substring(0, counter) + "_");
+                    e[0] = 5;
+                } else {
+                    label.setText(text.substring(0, counter));
+                    e[0] --;
+                }
                 counter++;
             }
         }));
-        timeline.setCycleCount(text.length() + 1);
-        timeline.play();
+        /*
+        消失：
+        counter = text.length();
+        Timeline in = new Timeline(new KeyFrame(Duration.millis(80), event -> {
+            if (counter <= text.length()) {
+                label.setText(text.substring(0, counter));
+                counter--;
+            }
+        }));*/
+        in.setCycleCount(text.length() + 1);
+        in.play();
     }
 
     public static void main(String[] args) {
